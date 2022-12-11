1e77675a11a55ea355de326d00a5cd012b4aa3e9
Clint Harrison <243356+clintharrison@users.noreply.github.com>
2022-12-11 10:24:55 -0500

    Use a Solution class

--- day1.rb
+++ day1.rb
@@ -1,18 +1,20 @@
 # typed: strict
 require "./util.rb"
 
-sig { params(input: T::Array[String]).returns(T.untyped) }
-def part1(input)
-  chunks = input.chunk { |s| s.empty? ? nil : true }.map { _2.map(&:to_i) }
-  chunks.map(&:sum).max
-end
+class Solution < BaseSolution
+  sig { override.params(input: T::Array[String]).returns(T.untyped) }
+  def part1(input)
+    chunks = input.chunk { |s| s.empty? ? nil : true }.map { _2.map(&:to_i) }
+    chunks.map(&:sum).max
+  end
 
-sig { params(input: T::Array[String]).returns(T.untyped) }
-def part2(input)
-  chunks = input.chunk { |s| s.empty? ? nil : true }.map { _2.map(&:to_i) }
-  chunks.map(&:sum).sort.last(3).sum
+  sig { override.params(input: T::Array[String]).returns(T.untyped) }
+  def part2(input)
+    chunks = input.chunk { |s| s.empty? ? nil : true }.map { _2.map(&:to_i) }
+    chunks.map(&:sum).sort.last(3).sum
+  end
 end
 
 if __FILE__ == $0
-  run
+  Solution.run
 end
--- util.rb
+++ util.rb
@@ -1,53 +1,78 @@
 # typed: strict
 require "sorbet-runtime"
 
-extend(T::Sig)
-
-sig { params(sample: T::Boolean).returns(String) }
-def filename(sample = false)
-  filename = "#{day}#{"s" if sample}.txt"
+class ::Object
+  extend T::Sig
 end
 
-sig { params(sample: T::Boolean).returns(T::Array[String]) }
-def input(sample = false)
-  File.readlines(filename(sample), chomp: true)
-end
+class BaseSolution
+  extend T::Helpers
+  abstract!
 
-sig { returns(T::Array[String]) }
-def sample_input
-  input(true)
-end
+  sig { abstract.params(input: T::Array[String]).returns(T.untyped) }
+  def part1(input)
+  end
 
-sig { returns(String) }
-def day
-  File.basename($0, ".rb")
-end
+  sig { abstract.params(input: T::Array[String]).returns(T.untyped) }
+  def part2(input)
+  end
 
-sig { void }
-def run
-  puts(
-    <<~END
-        Running for #{day}!
-      ### Sample input ###
-      Part 1:
-      #{part1(sample_input)}
-      ----------
-      Part 2:
-      #{part2(sample_input)}
-    END
-  )
+  sig { params(sample: T::Boolean).returns(String) }
+  def filename(sample = false)
+    filename = "#{day}#{"s" if sample}.txt"
+  end
 
-  if File.exists?(filename)
+  sig { params(sample: T::Boolean).returns(T::Array[String]) }
+  def input(sample = false)
+    File.readlines(filename(sample), chomp: true)
+  end
+
+  sig { returns(T::Array[String]) }
+  def sample_input
+    input(true)
+  end
+
+  sig { returns(String) }
+  def day
+    File.basename($0, ".rb")
+  end
+
+  sig { void }
+  def run
+    if !File.exists?(filename(sample = true))
+      puts("Expected the sample input #{filename(true)} to exist, exiting...")
+      exit(1)
+    end
+
     puts(
       <<~END
-
-        ### Real input ###
+          Running for #{day}!
+        ### Sample input ###
         Part 1:
-        #{part1(input)}
+        #{part1(sample_input)}
         ----------
         Part 2:
-        #{part2(input)}
+        #{part2(sample_input)}
       END
     )
+
+    if File.exists?(filename)
+      puts(
+        <<~END
+
+          ### Real input ###
+          Part 1:
+          #{part1(input)}
+          ----------
+          Part 2:
+          #{part2(input)}
+        END
+      )
+    end
+  end
+
+  sig { void }
+  def self.run
+    self.new.run
   end
 end
