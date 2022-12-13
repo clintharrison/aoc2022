53db696f92f1a0ae229134a7d3dca25e31d391d1
Clint Harrison <243356+clintharrison@users.noreply.github.com>
2022-12-12 22:59:50 -0500

    template

---
+++ template.rb
@@ -0,0 +1,16 @@
+# typed: strict
+require_relative "./util.rb"
+
+class Solution < BaseSolution
+  sig { override.params(input: T::Array[String]).returns(T.untyped) }
+  def part1(input)
+  end
+
+  sig { override.params(input: T::Array[String]).returns(T.untyped) }
+  def part2(input)
+  end
+end
+
+if __FILE__ == $0
+  Solution.run
+end
